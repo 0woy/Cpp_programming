4110da2dcb0f3e94c3ded0b3e4998417585ec168
0woy <qkrdbsdk1030@naver.com>
2022-10-17 16:47:26 +0900

    Ch5 10번 - 참조 리턴

---
+++ 과제/hw02_Q10.cpp
@@ -0,0 +1,32 @@
+/*
+이름: 박윤아
+학번: 20204624
+제출일: 2022.10.15.
+
+5장 연습문제 10번: 
+참조를 리턴하는 코드 작성
+*/
+#include<iostream>
+using namespace std;
+class Buffer {
+	string text;
+public:
+	Buffer(string text) { this->text = text; }
+	void add(string next) { text += next; }
+	void pirnt() { cout << text << endl; }
+};
+
+// orig의 값도 바뀌어야 하므로 참조 매개변수로 설정
+Buffer& append(Buffer&orig, string text) {
+	orig.add(text);
+	return orig;
+}
+
+int main() {
+
+	cout << "이름: 박윤아\n학번:20204624\n" << endl;
+	Buffer buf("Hello");
+	Buffer& temp = append(buf, "Guys");
+	temp.pirnt();
+	buf.pirnt();
+}
