bd403cd6936aeb68d49102a800f22ea7f745a885
0woy <qkrdbsdk1030@naver.com>
2022-12-12 13:58:40 +0900

    ifstream, ofstream 활용하여 텍스트 파일 읽기, 쓰기

--- Ch12/Q1-4.cpp
+++ Ch12/Q1-4.cpp
@@ -6,62 +6,42 @@ using namespace std;
 
 int main() {
 
-	ifstream fin("C:\\Temp\\test.txt");
+	ifstream fin;
+	fin.open("C://Windows//system.ini", ios::in);
 	
-	if (!fin) {
-		cout << "열기 실패";
-		return 0;
+	fstream fout("C://Temp//test.txt", ios::app);
 
-	}
-
-	int ch;
-/*	while ((ch = fin.get()) != EOF) { // fin으로 읽어서 ch에 저장후 ch가 EOF가 아니라면 반복
-		cout.put(ch); //1byte 씩 출력(1byte 씩 읽음)
-	}
-	
-
-	ofstream fout("C:\\Temp\\out.txt"); //파일에 출력하는 함수
-	if (!fout) {
-		cout << "열기 실패";
+	if (!fin || !fout) {
+		cout << "파일 열기 오류" << endl;
 		return 0;
 	}
-	
 
-	// Q2
-	string line;
+	// 영문 텍스트 파일을 읽고 모두 대문자로 변환하여 파일에 저장
+	char ch;
+	while (true) {
+		fin.get(ch);
+		if (fin.eof()) break;
+		if (islower(ch)) ch = toupper(ch);
+		fout.put(ch);
+	}
+	
+	// 파일을 읽고 라인 번호를 붙여서 출력
+	/* fin.getline(char* str, int n) 이용
+ 	int no = 1;
+	char line[100];
+	while (fin.getline(line, 100)) {
+		cout <<no++<<" : "<< line << endl;
+	}*/
+
+	// string의 getline() 이용
 	int no = 1;
-	while (getline(fin, line)) { //cin이 아닌 fin으로 해주어야함.
-		//fout << line << endl;
-		cout << no++ << ": " << line << endl;
+	string line;
+	while (getline(fin, line)) {
+		cout << no++ << " : " << line << endl;
 	}
-	
 
-	// Q3 한 글자씩 읽어서 영문 소문자를 대문자로 변경(islower, toupper 사용)
-	while ((ch = fin.get()) != EOF) {
-		if (islower(ch)) {
-			cout.put(toupper(ch));
-		}
-		else
-			cout.put(ch);
-		// = cout <<(char)ch;
-	}
-	*/
 
-	//Q4 파일에다 출력
-	ofstream fout("C:\\Temp\\out.txt",ios::app); //ios:app 뒤에 이어서 씀
-	
-	while ((ch = fin.get()) != EOF) {
-		if (islower(ch)) {
-			fout.put(toupper(ch));
-		}
-		else
-			fout.put(ch);
-		// = cout <<(char)ch;
-	}
-	
-	
 
-	fin.close();
-	//fout.close();
 
+	return 0;
 }
